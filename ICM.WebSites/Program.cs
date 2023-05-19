3ce416a73c395c2efff097d04a63bbe09919ee01
Vit Mihaescu <vit.mihaescu@gmail.com>
2023-05-19 19:31:17 +0100

    Clean up

--- ICM.WebSites/Program.cs
+++ ICM.WebSites/Program.cs
@@ -1,22 +1,19 @@
-namespace ICM.WebSites;
-
-public class Program
+namespace ICM.WebSites
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        CreateHostBuilder(args)
-            .Build()
-            .Run();
-    }
+        public static void Main(string[] args)
+            => CreateHostBuilder(args)
+                .Build()
+                .Run();
 
-    public static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return Host.CreateDefaultBuilder(args)
-            .ConfigureUmbracoDefaults()
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStaticWebAssets();
-                webBuilder.UseStartup<Startup>();
-            });
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureUmbracoDefaults()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStaticWebAssets();
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
