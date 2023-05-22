7052ff469384b6fb8b2f9adc60f1e8e74897107a
Vit Mihaescu <vit.mihaescu@gmail.com>
2023-05-22 16:22:37 +0100

    Improve MarketNewsEmailTemplate

---
+++ ICM.WebSites/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PublishedCache;
+
+namespace ICM.WebSites.Extensions;
+
+public static class PublishedContentExtensions
+{
+    public static IPublishedContent? GetByKey(this IPublishedCache? content, string guid)
+    {
+        return content?.GetById(new Guid(guid));
+    }
+}
---
+++ ICM.WebSites/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ICM.WebSites.Extensions;
+
+public static partial class StringExtensions
+{
+    public static string? RemoveSpaces(this string? value)
+    {
+        return value != null ? WhiteSpaceRegex().Replace(value, "") : null;
+    }
+
+    [GeneratedRegex("\\s+")]
+    private static partial Regex WhiteSpaceRegex();
+}
