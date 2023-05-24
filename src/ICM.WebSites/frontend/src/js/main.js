52f5dbb62d007de3e2b63c04d623bee2b0645117
Vit Mihaescu <vit.mihaescu@gmail.com>
2023-05-24 14:56:08 +0100

    Install bootstrap

---
+++ src/ICM.WebSites/frontend/src/js/main.js
@@ -0,0 +1,5 @@
+﻿// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
---
+++ src/ICM.WebSites/frontend/vite.config.js
@@ -0,0 +1,12 @@
+﻿const path = require('path')
+
+export default {
+    root: path.resolve(__dirname, 'src'),
+    build: {
+        outDir: '../dist'
+    },
+    server: {
+        port: 8080,
+        hot: true
+    }
+}
