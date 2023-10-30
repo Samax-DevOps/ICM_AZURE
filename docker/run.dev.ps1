3332536acfeb2365f6a623b55531235b6575c537
Vit Mihaescu <vit.mihaescu@gmail.com>
2023-10-30 10:44:03 +0000

    Update deployment scripts

---
+++ docker/run.dev.ps1
@@ -0,0 +1,8 @@
+docker compose down
+docker --context dev build --pull -f Dockerfile -t icm/websites ../src
+docker --context dev compose up -d
+docker --context dev rmi $(docker images -f “dangling=true” -q)
+
+
+# login into container
+# docker --context dev exec -it icm/websites sh
--- docker/run.local.ps1
+++ docker/run.local.ps1
@@ -1,14 +1,8 @@
+
 docker compose down
 docker build --pull -f Dockerfile -t icm/websites ../src
 docker compose up
 docker rmi $(docker images -f “dangling=true” -q)
 
-
-# build docker image
-#docker build --pull -f Dockerfile -t icm/websites ../src
-
-# run with compose 
-# docker-compose up -d
-
 # login into container
-# docker run -it --rm icm/websites /bin/ash
+# docker exec -it icm-websites-staging sh
