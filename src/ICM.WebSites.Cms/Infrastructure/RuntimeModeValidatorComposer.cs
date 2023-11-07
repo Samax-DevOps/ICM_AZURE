8bfa2ce7d300d7d691abed4e9e5cddb34080f6e0
Vit Mihaescu <vit.mihaescu@gmail.com>
2023-11-07 15:34:51 +0000

    Update Staging settings

---
+++ src/ICM.WebSites.Cms/Infrastructure/RuntimeModeValidatorComposer.cs
@@ -0,0 +1,10 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Infrastructure.Runtime.RuntimeModeValidators;
+
+namespace ICM.WebSites.Cms.Infrastructure;
+
+public class RuntimeModeValidatorComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+        => builder.RuntimeModeValidators().Remove<UseHttpsValidator>();
+}
