4eb6ce040a17310330001d0b5ff68639adf28c2b
Vit Mihaescu <vit.mihaescu@gmail.com>
2024-05-20 10:02:10 +0100

    Configure docker

--- src/ICM.WebSites.Cms/wwwroot/assets/icmsolutions/js/contact-form.js
+++ src/ICM.WebSites.Cms/wwwroot/assets/icmsolutions/js/contact-form.js
@@ -50,7 +50,7 @@ $(document).ready(function () {
             flag = true;
         }
         const dataString = "name=" + name.val() + "&email=" + email.val() + "&phone=" + phone.val();
-        $(".loading").fadeIn("slow").html("Loading...");
+        $(".loading").fadeIn("slow").html("Sending the request...");
         $.ajax({
             type: "POST",
             data: dataString,
@@ -59,9 +59,9 @@ $(document).ready(function () {
             success: function (d) {
                 $(".form-control").removeClass("success");
                 if (d === 'success') // Message Sent? Show the 'Thank You' message and hide the form
-                    $('.loading').fadeIn('slow').html('<font color="#48af4b">Mail sent Successfully.</font>').delay(3000).fadeOut('slow');
+                    $('.loading').fadeIn('slow').html('<font color="#48af4b">Thank you! Your request has been submitted successfully.</font>').delay(3000).fadeOut('slow');
                 else
-                    $('.loading').fadeIn('slow').html('<font color="#ff5607">Mail not sent.</font>').delay(3000).fadeOut('slow');
+                    $('.loading').fadeIn('slow').html('<font color="#ff5607">Something went wrong. Please try again later.</font>').delay(3000).fadeOut('slow');
                 document.contactform.reset();
             }
         });
