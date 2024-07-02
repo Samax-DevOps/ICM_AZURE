9877c3c1ae398af8a99bdde3bfa4c28c1652b9c1
Vit Mihaescu <vit.mihaescu@gmail.com>
2024-07-02 13:08:18 +0100

    Configure ssl

--- docker/run.az-tools-srv-api02.ps1
+++ docker/run.az-tools-srv-api02.ps1
@@ -1,7 +1,7 @@
-docker compose down
+docker --context az-tools-srv-api02 compose down
 docker --context az-tools-srv-api02 build --pull -f Dockerfile -t icm/websites ../src
-docker --context az-tools-srv-api02 compose up -d
-docker --context az-tools-srv-api02 rmi $(docker images -f “dangling=true” -q)
+docker --context az-tools-srv-api02 compose up #-d
+#docker --context az-tools-srv-api02 rmi $(docker images -f “dangling=true” -q)
 
 
 # login into container
